00b00b004df7fe37b6f93478b3489e7b5e9bd7da
OuOHoon <wls2579@gmail.com>
2023-12-13 14:31:03 +0900

    first commit

---
+++ src/main/java/me/ouohoon/basicreview/BasicReviewApplication.java
@@ -0,0 +1,13 @@
+package me.ouohoon.basicreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ouohoon/basicreview/BasicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ouohoon.basicreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
