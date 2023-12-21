41185f2c177f30b52d18f13291140768973deb97
OuOHoon <wls2579@gmail.com>
2023-12-21 22:00:12 +0900

    Feat(log): spring aop로 메서드 호출 로거 추가

---
+++ src/main/java/me/ouohoon/basicreview/global/log/LoggingAspect.java
@@ -0,0 +1,29 @@
+package me.ouohoon.basicreview.global.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(public * me.ouohoon.basicreview..*Controller.*(..)) " +
+            "|| execution(public * me.ouohoon.basicreview..*Service.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Method Start: {}.{}",
+                joinPoint.getSignature().getDeclaringType().getName(), joinPoint.getSignature().getName());
+        try {
+            Object proceed = joinPoint.proceed();
+            log.info("Method End: {}.{}",
+                    joinPoint.getSignature().getDeclaringType().getName(), joinPoint.getSignature().getName());
+            return proceed;
+        } catch (Throwable e) {
+            log.error("Method Exception:");
+            throw e;
+        }
+    }
+}
